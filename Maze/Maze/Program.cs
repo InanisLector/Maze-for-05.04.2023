deee2a9f3f4c975d2c152b4d22b545b486cc6af9
Inanis lector <kid@kde.kiev.ua>
2023-04-04 09:59:56 +0300

    Maze generation
    It generates mazes, even tho they quite linear

--- Maze/Maze/Program.cs
+++ Maze/Maze/Program.cs
@@ -20,7 +20,7 @@ class Program
             playerPosition.x = 1;
             playerPosition.y = 1;
 
-            map = new Map(10, 10);
+            map = new Map(30, 20);
             RenderMap();
             
             didntWin = true;
@@ -48,15 +48,15 @@ class Program
         Vector2 input = Console.ReadKey(true)
                 .Key switch
             {
-                ConsoleKey.W => new Vector2(0, -1),
-                ConsoleKey.UpArrow => new Vector2(0, -1),
-                ConsoleKey.A => new Vector2(-1, 0),
-                ConsoleKey.LeftArrow => new Vector2(-1, 0),
-                ConsoleKey.S => new Vector2(0, 1),
-                ConsoleKey.DownArrow => new Vector2(0, 1),
-                ConsoleKey.D => new Vector2(1, 0),
-                ConsoleKey.RightArrow => new Vector2(1, 0),
-                _ => new Vector2()
+                ConsoleKey.W => Vector2.Up,
+                ConsoleKey.UpArrow => Vector2.Up,
+                ConsoleKey.A => Vector2.Left,
+                ConsoleKey.LeftArrow => Vector2.Left,
+                ConsoleKey.S => Vector2.Down,
+                ConsoleKey.DownArrow => Vector2.Down,
+                ConsoleKey.D => Vector2.Right,
+                ConsoleKey.RightArrow => Vector2.Right,
+                _ => Vector2.Zero
             };
 
         inputCycle?.Invoke(input);
@@ -128,18 +128,99 @@ class Program
         {
             for (i.x = 0; i.x < width; i.x++)
             {
-                if (i.x == 0 || i.y == 0 || i.y == height - 1 || i.x == width - 1)
+                if (i.x == 0 || i.y == 0 || i.y == height - 1 || i.x == width - 1) // Borders
                 {
                     map[i.x, i.y] = '#';
                     continue;
                 }
 
+                if (i.x % 2 == 0 || i.y % 2 == 0) // Intersections
+                {
+                    map[i.x, i.y] = '#';
+                    continue;
+                }
+
+
                 map[i.x, i.y] = '.';
             }
         }
 
+        GenerateInnerWalls(map, width, height);
+
         map[width - 2, height - 2] = 'E';
 
+        return map;
+    }
+
+    private static char[,] GenerateInnerWalls(char[,] map, int width, int height)
+    {
+        int widthOfBool = width >> 1; // Division by 2
+        int heightOfBool = height >> 1;
+
+        bool[,] visited = new bool[widthOfBool, heightOfBool];
+        int toVisit = (widthOfBool) * (heightOfBool);
+
+        Vector2 currentPosition = new(0, 0);
+        Stack<Vector2> path = new Stack<Vector2>();
+        
+        path.Append(currentPosition);
+
+        Random rand = new();
+
+        int i = 1;
+
+        do
+        {
+            if (!visited[currentPosition.x, currentPosition.y])
+            {
+                toVisit--;
+                visited[currentPosition.x, currentPosition.y] = true;
+            }
+
+            var neighbors = new Vector2[4];
+            int availableNeighbors = 0;
+
+            if (currentPosition.y > 0)
+            {
+                (int x, int y) = (currentPosition + Vector2.Up);
+                if (!visited[x, y])
+                    neighbors[availableNeighbors++] = currentPosition + Vector2.Up;
+            }
+            if(currentPosition.y < heightOfBool - 1)
+            {
+                (int x, int y) = (currentPosition + Vector2.Down);
+                if (!visited[x, y])
+                    neighbors[availableNeighbors++] = currentPosition + Vector2.Down;
+            }
+            if (currentPosition.x > 0)
+            {
+                (int x, int y) = (currentPosition + Vector2.Left);
+                if (!visited[x, y])
+                    neighbors[availableNeighbors++] = currentPosition + Vector2.Left;
+            }
+            if (currentPosition.x < widthOfBool - 1)
+            {
+                (int x, int y) = (currentPosition + Vector2.Right);
+                if (!visited[x, y])
+                    neighbors[availableNeighbors++] = currentPosition + Vector2.Right;
+            }
+
+            if (availableNeighbors > 0)
+            {
+                path.Push(currentPosition);
+
+                currentPosition = neighbors[rand.Next(availableNeighbors)];
+                Vector2 addPathPosition = currentPosition + path.Peek() + new Vector2(1, 1); // Расчёты на бумаге были для координат, начиная с (1, 1)
+
+                map[addPathPosition.x, addPathPosition.y] = '.';
+            }
+            else
+            {
+                currentPosition = path.Pop();
+            }
+
+        } while (toVisit > 0);
+            
         return map;
     }
 
@@ -184,6 +265,13 @@ class Program
         public int x;
         public int y;
 
+        public static Vector2 Zero => new Vector2(0, 0);
+        public static Vector2 Up => new Vector2(0, -1);
+        public static Vector2 Down=> new Vector2(0, 1);
+        public static Vector2 Left => new Vector2(-1, 0);
+        public static Vector2 Right => new Vector2(1, 0);
+
+
         public Vector2(int x, int y)
         {
             this.x = x;
@@ -199,11 +287,27 @@ class Program
         public static Vector2 operator *(Vector2 v1, int multiplier)
             => new Vector2(v1.x * multiplier, v1.y * multiplier);
 
+        //public static Vector2 operator /(Vector2 v1, int divisor)
+        //{
+        //    float multiplier = 1f / divisor;
+
+        //    return new Vector2((int)(v1.x * multiplier), (int)(v1.y * multiplier));
+        //}
+
         public static bool operator ==(Vector2 v1, Vector2 v2)
             => v1.x == v2.x && v1.y == v2.y;
 
         public static bool operator !=(Vector2 v1, Vector2 v2)
             => !(v1 == v2);
+
+        public override string ToString()
+            => $"({x}, {y})";
+
+        public void Deconstruct(out int x, out int y)
+        {
+            x = this.x;
+            y = this.y;
+        }
     }
 
     #endregion
